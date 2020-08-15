7d954000902d272a6659b832129b24772e0f430d
Juan Perafan <perafju@cronos.be>
2020-08-15 16:48:21 +0200

    Now it is even more consize: Using choices instead of choice

--- random_password_generator.py
+++ random_password_generator.py
@@ -7,4 +7,4 @@ def generate_password(password_length = 20):
     all_possible_characters = ascii_letters + digits + punctuation
 
     # Choose a random character and concatenate it into a list of length == password length
-    return "".join(choice(all_possible_characters) for character in range(password_length))
+    return "".join(choices(all_possible_characters, k = password_length))
